39809238447b291f7710e6d4d25dbd7e37d89fee
WinterThane <demonsaso@gmail.com>
2020-04-20 10:42:56 +0200

    Test

---
+++ Bombyx2/Bombyx2/Bombyx2Component.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Grasshopper.Kernel;
+
+namespace Bombyx2
+{
+    public class Bombyx2Component : GH_Component
+    {
+        public Bombyx2Component()
+          : base("Bombyx 2", 
+                 "Bombyx 2",
+                 "Bombyx description",
+                 "Bombyx 2", 
+                 "Base")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("8823b024-6994-4ed5-b19b-627733283d75"); }
+        }
+    }
+}
---
+++ Bombyx2/Bombyx2/Bombyx2Info.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Bombyx2
+{
+    public class Bombyx2Info : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Bombyx2";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("5af956a8-f831-4691-9d71-621053263843");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}

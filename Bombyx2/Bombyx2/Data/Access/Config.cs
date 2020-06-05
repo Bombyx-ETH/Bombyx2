e8c45c34102a77f0a9276ee04f4d0f6c142c6f5f
WinterThane <demonsaso@gmail.com>
2020-06-05 07:56:59 +0200

    Fixed DB location

---
+++ Bombyx2/Bombyx2/Data/Access/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bombyx2.Data.Access
+{
+    public static class Config
+    {
+        private static string DBlocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Grasshopper\\Libraries\\Bombyx2.0\\";
+        private static string connectionString = "Data Source=" + DBlocation + "MaterialsDB.db; Version=3;";
+        //private static string connectionString = "Data Source=MaterialsDB.db; Version=3;";
+
+        public static string LoadConnectionString()
+        {
+            return connectionString;
+        }
+    }
+}

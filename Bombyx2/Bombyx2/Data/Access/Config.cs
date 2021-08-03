4c0c4f39c64d9f89c5aed863a3141f8a82c3e6b4
WinterThane <winterthane.2020@outlook.com>
2021-08-03 17:13:57 +0200

    version 2.0.8

--- Bombyx2/Bombyx2/Data/Access/Config.cs
+++ Bombyx2/Bombyx2/Data/Access/Config.cs
@@ -4,7 +4,7 @@ namespace Bombyx2.Data.Access
 {
     public static class Config
     {
-        public static readonly string Version = "2.0.7";
+        public static readonly string Version = "2.0.8";
         private static string DBlocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Grasshopper\\Libraries\\Bombyx" + Version + "\\";
         private static string connectionString = "Data Source=" + DBlocation + "MaterialsDB.db; Version=3;";
         //private static string connectionString = "Data Source=MaterialsDB.db; Version=3;";
